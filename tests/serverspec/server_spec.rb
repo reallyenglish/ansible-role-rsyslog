857f572ba370a6d9ff2af64efceb21d115887d34
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-10-21 11:34:02 +0900

    implement ACL on remote server

---
+++ tests/serverspec/server_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+rsyslog_package_name = 'rsyslog'
+rsyslog_service_name = 'rsyslog'
+rsyslog_user_name    = 'root'
+rsyslog_user_group   = 'root'
+rsyslog_work_dir     = '/var/spool/rsyslog'
+rsyslog_config_dir   = '/etc/rsyslog.d'
+rsyslog_config_path  = '/etc/rsyslog.conf'
+
+os_default_syslog_service_name = nil
+
+case os[:family]
+when 'freebsd'
+  rsyslog_service_name = 'rsyslogd'
+  rsyslog_config_path  = '/usr/local/etc/rsyslog.conf'
+  rsyslog_config_dir   = '/usr/local/etc/rsyslog.d'
+  rsyslog_user_name    = 'root'
+  rsyslog_user_group   = 'wheel'
+  os_default_syslog_service_name = 'syslogd'
+end
+
+rsyslog_server_config_path = "#{ rsyslog_config_dir }/400_server.cfg"
+
+
+describe package(rsyslog_package_name) do
+  it { should be_installed }
+end 
+
+describe service(rsyslog_service_name) do
+  it { should be_running }
+  it { should be_enabled }
+end
+
+# XXX due to bug in serverspec, these tests do not work
+if os_default_syslog_service_name && 0 == 1
+  describe service(os_default_syslog_service_name) do
+    it { should_not be_running }
+    it { should_not be_enabled }
+  end
+end
+
+describe port(514) do
+  it { should be_listening }
+end
+
+describe file(rsyslog_config_path) do
+  it { should be_file }
+  its(:content) { should match Regexp.escape("$FileOwner #{rsyslog_user_name}") }
+  its(:content) { should match Regexp.escape("$FileGroup #{rsyslog_user_group}") }
+  its(:content) { should match Regexp.escape("$WorkDirectory #{rsyslog_work_dir}") }
+  its(:content) { should match Regexp.escape("$IncludeConfig #{rsyslog_config_dir}/*.cfg") }
+end
+
+describe file(rsyslog_server_config_path) do
+  it { should be_file }
+  its(:content) { should match Regexp.escape('$AllowedSender UDP, 192.168.0.0/16') }
+  its(:content) { should match Regexp.escape('$AllowedSender TCP, 192.168.0.0/16') }
+end
