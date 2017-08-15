c458ce436df2031ce92202812845e9d19d8b9e2e
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-08-15 16:20:31 +0900

    [bugfix] QA and bump CentOS box version

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,53 +1,57 @@
-require 'spec_helper'
+require "spec_helper"
 
-rsyslog_package_name = 'rsyslog'
-rsyslog_service_name = 'rsyslog'
-rsyslog_user_name    = 'root'
-rsyslog_user_group   = 'root'
-rsyslog_work_dir     = '/var/spool/rsyslog'
-rsyslog_config_dir   = '/etc/rsyslog.d'
-rsyslog_config_path  = '/etc/rsyslog.conf'
-rsyslog_default_log_file = '/var/log/messages'
+rsyslog_package_name = "rsyslog"
+rsyslog_service_name = "rsyslog"
+rsyslog_user_name    = "root"
+rsyslog_user_group   = "root"
+rsyslog_work_dir     = "/var/spool/rsyslog"
+rsyslog_config_dir   = "/etc/rsyslog.d"
+rsyslog_config_path  = "/etc/rsyslog.conf"
+rsyslog_default_log_file = "/var/log/messages"
 
 os_default_syslog_service_name = nil
 
 case os[:family]
-when 'freebsd'
-  rsyslog_service_name = 'rsyslogd'
-  rsyslog_config_path  = '/usr/local/etc/rsyslog.conf'
-  rsyslog_config_dir   = '/usr/local/etc/rsyslog.d'
-  rsyslog_user_name    = 'root'
-  rsyslog_user_group   = 'wheel'
-  os_default_syslog_service_name = 'syslogd'
-when 'openbsd'
-  rsyslog_service_name = 'rsyslogd'
-  rsyslog_user_name    = 'root'
-  rsyslog_user_group   = 'wheel'
-  os_default_syslog_service_name = 'syslogd'
-when 'ubuntu'
-  rsyslog_default_log_file = '/var/log/syslog'
+when "freebsd"
+  rsyslog_service_name = "rsyslogd"
+  rsyslog_config_path  = "/usr/local/etc/rsyslog.conf"
+  rsyslog_config_dir   = "/usr/local/etc/rsyslog.d"
+  rsyslog_user_name    = "root"
+  rsyslog_user_group   = "wheel"
+  os_default_syslog_service_name = "syslogd"
+when "openbsd"
+  rsyslog_service_name = "rsyslogd"
+  rsyslog_user_name    = "root"
+  rsyslog_user_group   = "wheel"
+  os_default_syslog_service_name = "syslogd"
+when "ubuntu"
+  rsyslog_default_log_file = "/var/log/syslog"
 end
-
 
 describe package(rsyslog_package_name) do
   it { should be_installed }
-end 
+end
 
 describe service(rsyslog_service_name) do
   it { should be_running }
   it { should be_enabled }
 end
 
-# XXX due to bug in serverspec, these tests do not work
-if os_default_syslog_service_name && 0 == 1
+if os_default_syslog_service_name
   describe service(os_default_syslog_service_name) do
-    it { should_not be_running }
-    it { should_not be_enabled }
+    it do
+      pending "due to bug in serverspec, these tests do not work"
+      should_not be_running
+    end
+    it do
+      pending "due to bug in serverspec, these tests do not work"
+      should_not be_enabled
+    end
   end
 end
 
 case os[:family]
-when 'openbsd'
+when "openbsd"
   describe port(514) do
     it { should be_listening }
   end
@@ -67,11 +71,11 @@ end
 
 describe file("#{rsyslog_config_dir}/200_client.cfg") do
   regex_to_test = [
-    '$ActionQueueType LinkedList',
-    '$ActionQueueFileName 10.0.2.115:514-queue',
-    '$ActionResumeRetryCount -1',
-    '$ActionQueueSaveOnShutdown on',
-    '*.* @@10.0.2.115:514;RSYSLOG_ForwardFormat'
+    "$ActionQueueType LinkedList",
+    "$ActionQueueFileName 10.0.2.115:514-queue",
+    "$ActionResumeRetryCount -1",
+    "$ActionQueueSaveOnShutdown on",
+    "*.* @@10.0.2.115:514;RSYSLOG_ForwardFormat"
   ]
   it { should be_file }
   regex_to_test.each do |r|
@@ -79,11 +83,9 @@ describe file("#{rsyslog_config_dir}/200_client.cfg") do
   end
 end
 
-case os[:family]
-when 'openbsd'
-  # rsyslog package does not install imfile
-else
-  describe file('/tmp/dummy.log') do
+# rsyslog package does not install imfile
+if os[:family] != "openbsd"
+  describe file("/tmp/dummy.log") do
     it { should be_file }
   end
 
@@ -94,7 +96,7 @@ else
   #   Facility="local1"
   # )
 
-  describe file("#{ rsyslog_config_dir }/900_dummy.log.cfg") do
+  describe file("#{rsyslog_config_dir}/900_dummy.log.cfg") do
     it { should be_file }
     its(:content) { should match Regexp.escape('File="/tmp/dummy.log"') }
     its(:content) { should match(/Tag="dummy"/) }
@@ -103,14 +105,14 @@ else
 end
 
 sig = Digest::SHA256.hexdigest(Time.new.to_i.to_s)
-describe command("logger #{ Shellwords.escape(sig) }") do
-  its(:stderr) { should eq '' }
-  its(:stdout) { should eq '' }
+describe command("logger #{Shellwords.escape(sig)}") do
+  its(:stderr) { should eq "" }
+  its(:stdout) { should eq "" }
   its(:exit_status) { should eq 0 }
 end
 
-describe command("grep #{ Shellwords.escape(sig) } #{ Shellwords.escape(rsyslog_default_log_file) }") do
-  its(:stderr) { should eq '' }
+describe command("grep #{Shellwords.escape(sig)} #{Shellwords.escape(rsyslog_default_log_file)}") do
+  its(:stderr) { should eq "" }
   its(:stdout) { should match(/#{ Regexp.escape(sig) }/) }
   its(:exit_status) { should eq 0 }
 end
--- tests/serverspec/server_spec.rb
+++ tests/serverspec/server_spec.rb
@@ -1,47 +1,51 @@
-require 'spec_helper'
+require "spec_helper"
 
-rsyslog_package_name = 'rsyslog'
-rsyslog_service_name = 'rsyslog'
-rsyslog_user_name    = 'root'
-rsyslog_user_group   = 'root'
-rsyslog_work_dir     = '/var/spool/rsyslog'
-rsyslog_config_dir   = '/etc/rsyslog.d'
-rsyslog_config_path  = '/etc/rsyslog.conf'
-rsyslog_ports        = [ 514 ]
+rsyslog_package_name = "rsyslog"
+rsyslog_service_name = "rsyslog"
+rsyslog_user_name    = "root"
+rsyslog_user_group   = "root"
+rsyslog_work_dir     = "/var/spool/rsyslog"
+rsyslog_config_dir   = "/etc/rsyslog.d"
+rsyslog_config_path  = "/etc/rsyslog.conf"
+rsyslog_ports        = [514]
 
 os_default_syslog_service_name = nil
 
 case os[:family]
-when 'freebsd'
-  rsyslog_service_name = 'rsyslogd'
-  rsyslog_config_path  = '/usr/local/etc/rsyslog.conf'
-  rsyslog_config_dir   = '/usr/local/etc/rsyslog.d'
-  rsyslog_user_name    = 'root'
-  rsyslog_user_group   = 'wheel'
-  os_default_syslog_service_name = 'syslogd'
-when 'openbsd'
-  rsyslog_service_name = 'rsyslogd'
-  rsyslog_user_group   = 'wheel'
-  rsyslog_ports        = [ 5140 ]
+when "freebsd"
+  rsyslog_service_name = "rsyslogd"
+  rsyslog_config_path  = "/usr/local/etc/rsyslog.conf"
+  rsyslog_config_dir   = "/usr/local/etc/rsyslog.d"
+  rsyslog_user_name    = "root"
+  rsyslog_user_group   = "wheel"
+  os_default_syslog_service_name = "syslogd"
+when "openbsd"
+  rsyslog_service_name = "rsyslogd"
+  rsyslog_user_group   = "wheel"
+  rsyslog_ports        = [5140]
 end
 
-rsyslog_server_config_path = "#{ rsyslog_config_dir }/400_server.cfg"
-
+rsyslog_server_config_path = "#{rsyslog_config_dir}/400_server.cfg"
 
 describe package(rsyslog_package_name) do
   it { should be_installed }
-end 
+end
 
 describe service(rsyslog_service_name) do
   it { should be_running }
   it { should be_enabled }
 end
 
-# XXX due to bug in serverspec, these tests do not work
-if os_default_syslog_service_name && 0 == 1
+if os_default_syslog_service_name
   describe service(os_default_syslog_service_name) do
-    it { should_not be_running }
-    it { should_not be_enabled }
+    it do
+      pending "due to bug in serverspec, these tests do not work"
+      should_not be_running
+    end
+    it do
+      pending "due to bug in serverspec, these tests do not work"
+      should_not be_enabled
+    end
   end
 end
 
@@ -65,24 +69,22 @@ describe file(rsyslog_server_config_path) do
   rsyslog_ports.each do |port|
     udp_re = [
       'module(load="imudp")',
-      'input(',
+      "input(",
       '    type="imudp"',
-      "    port=\"#{ port }\"",
-      ')',
+      "    port=\"#{port}\"",
+      ")"
     ].map { |i| Regexp.escape(i) }.join('\n')
-    if os[:family] != 'openbsd'
-      its(:content) { should match(/#{ udp_re }/) }
-    end
+    its(:content) { should match(/#{ udp_re }/) } if os[:family] != "openbsd"
     tcp_re = [
       'module(load="imtcp")',
-      'input(',
+      "input(",
       '    type="imtcp"',
-      "    port=\"#{ port }\"",
-      ')',
+      "    port=\"#{port}\"",
+      ")"
     ].map { |i| Regexp.escape(i) }.join('\n')
     its(:content) { should match(/#{ tcp_re }/) }
   end
 
-  its(:content) { should match Regexp.escape('$AllowedSender UDP, 192.168.0.0/16') }
-  its(:content) { should match Regexp.escape('$AllowedSender TCP, 192.168.0.0/16') }
+  its(:content) { should match Regexp.escape("$AllowedSender UDP, 192.168.0.0/16") }
+  its(:content) { should match Regexp.escape("$AllowedSender TCP, 192.168.0.0/16") }
 end
